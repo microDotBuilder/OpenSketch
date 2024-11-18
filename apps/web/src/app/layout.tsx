287d2c0bffe439da355425ce00823b06729b0430
boringtutor <micro.builder.io@gmail.com>
2024-11-17 23:31:01 -0800

    still working on it

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+
 import "@repo/ui/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -17,7 +18,7 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-background`}>{children}</body>
     </html>
   );
 }
--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,12 +1,85 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
-// We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
+  darkMode: ["class"],
   theme: {
     extend: {
-      backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+      colors: {
+        border: "hsl(240 3.7% 15.9%)",
+        input: "hsl(240 3.7% 15.9%)",
+        ring: "hsl(240 3.7% 15.9%)",
+        background: "hsl(240 10% 3.9%)",
+        foreground: "hsl(0 0% 98%)",
+        primary: {
+          DEFAULT: "hsl(242 84% 82%)",
+          foreground: "hsl(240 10% 3.9%)",
+        },
+        secondary: {
+          DEFAULT: "hsl(240 3.7% 15.9%)",
+          foreground: "hsl(0 0% 98%)",
+        },
+        destructive: {
+          DEFAULT: "hsl(0 84% 82%)",
+          foreground: "hsl(0 0% 98%)",
+        },
+        muted: {
+          DEFAULT: "hsl(240 3.7% 15.9%)",
+          foreground: "hsl(240 5% 64.9%)",
+        },
+        accent: {
+          DEFAULT: "hsl(240 3.7% 15.9%)",
+          foreground: "hsl(0 0% 98%)",
+        },
+        popover: {
+          DEFAULT: "hsl(240 10% 3.9%)",
+          foreground: "hsl(0 0% 98%)",
+        },
+        card: {
+          DEFAULT: "hsl(240 10% 3.9%)",
+          foreground: "hsl(0 0% 98%)",
+        },
+        // Custom Excalidraw colors
+        success: {
+          DEFAULT: "hsl(122 39% 89%)",
+          foreground: "hsl(122 39% 22%)",
+        },
+        warning: {
+          DEFAULT: "hsl(37 92% 89%)",
+          foreground: "hsl(37 92% 22%)",
+        },
+        surface: {
+          low: "hsl(240 8% 15%)",
+          mid: "hsl(240 6% 10%)",
+          high: "hsl(245 10% 21%)",
+          lowest: "hsl(0 0% 7%;)",
+        },
+        brand: {
+          hover: "hsl(242 100% 86%)",
+          active: "hsl(242 100% 90%)",
+        },
+      },
+      borderRadius: {
+        lg: "0.5rem",
+        md: "calc(0.5rem - 2px)",
+        sm: "calc(0.5rem - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
